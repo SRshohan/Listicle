b2641e298269b7e41a9915a39935eb8b05093c9a
SRshohan <srshohan02@gmail.com>
2024-09-26 09:12:02 -0400

    Added changes

--- server.js
+++ server.js
@@ -6,17 +6,22 @@ const companies = require('./data')
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Set EJS as the template engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 // Middleware for serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Route for displaying all the companies
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+    // res.sendFile(path.join(__dirname, 'views', 'index.html'))
+    res.render('index', { companies });
 })
 
 app.get('/company/:id', (req, res) => {
     const company = companies.find(c => c.id === parseInt(req.params.id));
-    res.json(company)
+    res.render('company', { company });
 })
 
 // Middleware for handling 404 errors
